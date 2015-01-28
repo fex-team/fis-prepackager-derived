1e8eda4b0b1413859ce65ee9bf63a779ca7c0da6
2betop <2betop.cn@gmail.com>
2015-01-28 10:37:25 +0800

    rm token replace function

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 module.exports = function(ret, conf, settings, opt) {
-    var files = [];
 
     fis.util.map(ret.src, function(subpath, file){
 
@@ -7,44 +6,11 @@ module.exports = function(ret, conf, settings, opt) {
             return;
         }
 
-        files.push(file);
-
         file.extras.derived.forEach(function(obj) {
             obj.__proto__ = file.__proto__;
             ret.pkg[obj.subpath] = obj;
-
-            files.push(obj);
         });
 
         delete file.extras.derived;
-    });
-
-    // 转换 {{prop:subpath}} 变量。
-    files.forEach(function(file) {
-        var content = file.getContent();
-
-        content  = content.replace(/\{\{(\w+)\:(.+?)\}\}/g, function(_, prop, subpath) {
-            var target = ret.src[subpath] || ret.pkg[subpath];
-
-            if (!target) {
-                return _;
-            }
-
-            var val = '';
-
-            switch (prop) {
-                case 'url':
-                    val = target.getUrl(opt.hash, opt.domain);
-                    break;
-
-                default:
-                    val = target[prop];
-                    return
-            }
-
-            return val;
-        });
-
-        file.setContent(content);
     });
 };
